91fa5766809b83906aa08f2dbd2d5032ac1339da
anusha <anusripvc202@gmail.com>
2026-07-03 17:23:40 +0530

    Show 2 rows of 6 cards (12 total) by default, hide rest behind View All

--- src/components/SolutionsGrid.jsx
+++ src/components/SolutionsGrid.jsx
@@ -81,9 +81,9 @@ const solutions = Object.entries(serviceDatabase).map(([key, val]) => ({
   ...val
 }));
 
-const VISIBLE_ROWS = 1;
+const VISIBLE_ROWS = 2;
 const COLS = 6;
-const DEFAULT_VISIBLE = VISIBLE_ROWS * COLS; // 6
+const DEFAULT_VISIBLE = VISIBLE_ROWS * COLS; // 12
 
 export default function SolutionsGrid({ searchTerm, activeCategory, onEnquire }) {
   const [showAll, setShowAll] = React.useState(false);

4c3521d59b206d7bffee85cfbfb819fc30f15610
Ganesh2100080007 <immadisettyganesh@gmail.com>
2026-07-03 11:37:31 +0530

    footer logo changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -69,11 +69,19 @@ export default function Footer() {
                   }}
                   className="social-icon-hover"
                 >
-                  <span style={{ fontSize: '0.85rem' }}>
-                    {platform === 'facebook' && '📘'}
-                    {platform === 'twitter' && '🐦'}
-                    {platform === 'linkedin' && '💼'}
-                    {platform === 'instagram' && '📸'}
+                  <span style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    {platform === 'facebook' && (
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/></svg>
+                    )}
+                    {platform === 'twitter' && (
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
+                    )}
+                    {platform === 'linkedin' && (
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"/></svg>
+                    )}
+                    {platform === 'instagram' && (
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
+                    )}
                   </span>
                 </a>
               ))}
